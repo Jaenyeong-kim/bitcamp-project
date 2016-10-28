160a908f3efea147c26f8fdf87de27fc58ce58ad
jned0317 <jned0317@naver.com>
2016-10-28 10:57:57 +0900

    Second

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -4,50 +4,58 @@ import java.util.Scanner;
 public class EduApp {
 
   public static void main(String[] args) {
-
     Scanner scan = new Scanner(System.in);
-    System.out.println("비트캠프 ㅎㅇㅎㅇ");
+    // 여러 명의 학생 정보를 입력하기 위한 레퍼런스 배열 생성.
+    ClassRoom[] classes = new ClassRoom[100];
+    int length = 0;   // 레퍼런스 배열, 몇 명의 강의실 정보를 저장 했는지
+                      // 몇 개의 강의실 정보가 들어 있는지
 
-    ClassRoom classOne = new ClassRoom("Java&DB강의실", "3층", 27, 12, 4, true);
-    ClassRoom classTwo = new ClassRoom();
 
-    System.out.println(classOne.className);
-    System.out.println(classOne.floorInfo);
-    System.out.println(classOne.capacity);
-    System.out.println(classOne.cabinetQty);
-    System.out.println(classOne.airconditionerQty);
-    System.out.println(classOne.projectBim);
+    // 반복해서 입력 받음
+    while (length < classes.length) {
+      ClassRoom c1 = new ClassRoom();
+      System.out.println("강의실 이름을 입력하세요.");
+      c1.className = scan.nextLine();
 
-    System.out.println("강의실 이름을 입력하세요.");
-    classTwo.className = scan.nextLine();
+      System.out.println("강의실 층을 입력하세요.");
+      c1.floorInfo = scan.nextLine();
 
-    System.out.println("강의실 층을 입력하세요.");
-    classTwo.floorInfo = scan.nextLine();
+      System.out.println("강의실 수용 인원을 입력하세요.");
+      c1.capacity = Integer.parseInt(scan.nextLine());
 
-    System.out.println("강의실 수용 인원을 입력하세요.");
-    classTwo.capacity = Integer.parseInt(scan.nextLine());
+      System.out.println("강의실 캐비닛 수를 입력하세요.");
+      c1.cabinetQty = Integer.parseInt(scan.nextLine());
 
-    System.out.println("강의실 캐비닛 수를 입력하세요.");
-    classTwo.cabinetQty = Integer.parseInt(scan.nextLine());
+      System.out.println("강의실 에어컨 수를 입력하세요.");
+      c1.airconditionerQty = Integer.parseInt(scan.nextLine());
 
-    System.out.println("강의실 에어컨 수를 입력하세요.");
-    classTwo.airconditionerQty = Integer.parseInt(scan.nextLine());
+      System.out.println("강의실 프로젝트 빔 여부 (Y/N)");
+      c1.projectBim = scan.nextLine().equals("Y") ? true : false;
 
-    System.out.println("강의실 프로젝트 빔 여부 (Y/N)");
-    classTwo.projectBim = scan.nextLine().equals("Y") ? true : false;
+      classes[length++] = c1;
 
-    System.out.println(classTwo.className);
-    System.out.println(classTwo.floorInfo);
-    System.out.println(classTwo.capacity);
-    System.out.println(classTwo.cabinetQty);
-    System.out.println(classTwo.airconditionerQty);
-    System.out.println(classTwo.projectBim);
+      System.out.println("계속 입력하시겠습니까?");
+      if (!scan.nextLine().equals("y")) {
+        break;
+      }
 
-    // String className;
-    // String floorInfo;
-    // int capacity;
-    // int cabinetQty;
-    // int airconditionerQty;
-    // boolean projectBim;
+    }
+    printClassRoom(classes, length);
+  }
+
+  //    System.out.println("비트캠프 ㅎㅇㅎㅇ");
+  //static void printClassRoom(ClassRoom[] classes, int length) {
+  static void printClassRoom(ClassRoom[] classes, int length) {
+
+    for (int i = 0; i < length; i++) {
+      ClassRoom c1 = classes[i];
+      System.out.printf("%s, %s, %s, %d, %d, %b\n",
+       c1.className
+      ,c1.floorInfo
+      ,c1.capacity
+      ,c1.cabinetQty
+      ,c1.airconditionerQty
+      ,c1.projectBim);
+    }
   }
 }
